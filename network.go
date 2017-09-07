6cdf8986cfacd45f8312f7d2cd5a2f8d6bb510d9
nilfit <nils.fitinghoff@gmail.com>
2017-09-07 12:51:36 +0200

    add basic UDP listening

--- network.go
+++ network.go
@@ -1,24 +1,44 @@
 package d7024e
 
+import (
+	"net"
+	"fmt"
+	"log"
+)
+
 type Network struct {
 }
 
 func Listen(ip string, port int) {
-	// TODO
+	b := make([]byte, 2048)
+	addrStr := fmt.Sprintf("%s:%d", ip, port)
+	addr := net.ResolveUDPAddr("udp", addrStr)
+	srv, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatalf("Error listening on %v: %v\n", addr, err)
+	}
+	for {
+		_, raddr, err := srv.ReadFromUDP(b)
+		if err != nil {
+			log.Printf("Error reading UDP: %v", err)
+		}
+		// TODO: unmarshal
+		// TODO: in new thread
+		// TODO: marshal response
+		// TODO: send it
+		go resolveRPC(b, raddr)
 }
 
 func (network *Network) SendPingMessage(contact *Contact) {
-	// TODO
 }
 
 func (network *Network) SendFindContactMessage(contact *Contact) {
-	// TODO
 }
 
 func (network *Network) SendFindDataMessage(hash string) {
-	// TODO
 }
 
 func (network *Network) SendStoreMessage(data []byte) {
-	// TODO
 }
+
+// TODO: Send{Ping, FindContact, FindData, Store}Response
