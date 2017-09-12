d30ff702887a13af22df36e43f594fbfb6eb2229
mjolnir92 <xerzeon@gmail.com>
2017-09-12 18:19:34 +0200

    Work on node lookups

--- kademlia/kademlia.go
+++ kademlia/kademlia.go
@@ -7,16 +7,22 @@ import (
 
 const {
 	ALPHA = 3
+	K = 20
 }
 
 type T struct {
+	//TODO: Create work dispatcher running <ALPHA> threads
+	//TODO: Create routing table
 }
 
 func (kademlia *T) LookupContact(target *contact.T) {
+	var candidates []contact.T
+	//TODO: Define queried map
 	closestNodes := routingtable.FindClosestContacts(target.ID, ALPHA)
 	for _, node := range closestNodes {
-		//TODO: Enqueue FIND_NODE call to <node>
+		//TODO: Enqueue FIND_NODE call to <node> and append to <candidates>
 	}
+	
 	//TODO: Repeat until response from k closest, remove queried from consideration
 }
 
