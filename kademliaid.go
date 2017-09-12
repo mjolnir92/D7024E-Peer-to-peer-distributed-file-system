6abd4d0206528ebe70dda4a867564f4543079b13
nilfit <nils.fitinghoff@gmail.com>
2017-09-12 15:45:25 +0200

    refactor for better names

--- kademliaid.go
+++ kademliaid.go
@@ -7,12 +7,12 @@ import (
 
 const IDLength = 20
 
-type KademliaID [IDLength]byte
+type T [IDLength]byte
 
-func NewKademliaID(data string) *KademliaID {
+func New(data string) *T {
 	decoded, _ := hex.DecodeString(data)
 
-	newKademliaID := KademliaID{}
+	newKademliaID := T{}
 	for i := 0; i < IDLength; i++ {
 		newKademliaID[i] = decoded[i]
 	}
@@ -20,15 +20,15 @@ func NewKademliaID(data string) *KademliaID {
 	return &newKademliaID
 }
 
-func NewRandomKademliaID() *KademliaID {
-	newKademliaID := KademliaID{}
+func NewRandom() *T {
+	newKademliaID := T{}
 	for i := 0; i < IDLength; i++ {
 		newKademliaID[i] = uint8(rand.Intn(256))
 	}
 	return &newKademliaID
 }
 
-func (kademliaID KademliaID) Less(otherKademliaID *KademliaID) bool {
+func (kademliaID T) Less(otherKademliaID *T) bool {
 	for i := 0; i < IDLength; i++ {
 		if kademliaID[i] != otherKademliaID[i] {
 			return kademliaID[i] < otherKademliaID[i]
@@ -37,7 +37,7 @@ func (kademliaID KademliaID) Less(otherKademliaID *KademliaID) bool {
 	return false
 }
 
-func (kademliaID KademliaID) Equals(otherKademliaID *KademliaID) bool {
+func (kademliaID T) Equals(otherKademliaID *T) bool {
 	for i := 0; i < IDLength; i++ {
 		if kademliaID[i] != otherKademliaID[i] {
 			return false
@@ -46,14 +46,14 @@ func (kademliaID KademliaID) Equals(otherKademliaID *KademliaID) bool {
 	return true
 }
 
-func (kademliaID KademliaID) CalcDistance(target *KademliaID) *KademliaID {
-	result := KademliaID{}
+func (kademliaID T) CalcDistance(target *T) *T {
+	result := T{}
 	for i := 0; i < IDLength; i++ {
 		result[i] = kademliaID[i] ^ target[i]
 	}
 	return &result
 }
 
-func (kademliaID *KademliaID) String() string {
+func (kademliaID *T) String() string {
 	return hex.EncodeToString(kademliaID[0:IDLength])
 }
