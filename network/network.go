41b87495f9bac265310403abc6bd3217dc44058b
nilfit <nils.fitinghoff@gmail.com>
2017-09-17 04:06:23 +0200

    fix network so it compiles, Ping almost works now

--- network/network.go
+++ network/network.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"bufio"
+	"reflect"
 	"github.com/mjolnir92/kdfs/kademliaid"
 	"github.com/mjolnir92/kdfs/contact"
 	"github.com/mjolnir92/kdfs/routingtable"
@@ -12,13 +14,13 @@ import (
 )
 
 type T struct {
-	id_local *kademliaid.T
+	id *kademliaid.T
 	routingtable *routingtable.T
 	timeout time.Duration
 }
 
-func New(timeoutms int64, id_local *kademliaid.T) T {
-	return T{timeout: timeoutms * time.Millisecond, id_local: id_local}
+func New(timeoutms int64, id *kademliaid.T) T {
+	return T{timeout: time.Duration(timeoutms) * time.Millisecond, id: id}
 }
 
 const (
@@ -28,89 +30,108 @@ const (
 	STORE = 3
 )
 
-type RPC struct {
-	rpc_type int
-	sender_id kademliaid.T
-}
+// type RPCHeader struct {
+// 	RPCType int
+// 	Sender kademliaid.T
+// }
 
 type RPCPing struct {
-	RPC
+	RPCType int
+	SenderID kademliaid.T
 }
 
 type RPCFindNode struct {
-	RPC
-	find_id kademliaid.T
+	RPCType int
+	SenderID kademliaid.T
+	FindID kademliaid.T
 }
 
 type RPCFindValue struct {
-	RPC
-	find_id kademliaid.T
+	RPCType int
+	SenderID kademliaid.T
+	FindID kademliaid.T
 }
 
 type RPCStore struct {
-	RPC
-	store_value []byte
+	RPCType int
+	SenderID kademliaid.T
+	StoreValue []byte
 }
 
-func Listen(ip string, port int) {
+func (nw *T) Listen(ip string, port int) {
 	b := make([]byte, 2048)
 	addrStr := fmt.Sprintf("%s:%d", ip, port)
-	addr := net.ResolveUDPAddr("udp", addrStr)
+	addr, err := net.ResolveUDPAddr("udp", addrStr)
+	if err != nil {
+		log.Fatalf("Error listening on %v: %v\n", addr, err)
+	}
 	srv, err := net.ListenUDP("udp", addr)
 	if err != nil {
 		log.Fatalf("Error listening on %v: %v\n", addr, err)
 	}
 	for {
+		log.Println("Server is ready to listen")
 		_, raddr, err := srv.ReadFromUDP(b)
+		log.Println("Server read something from UDP")
 		if err != nil {
 			log.Printf("Error reading UDP: %v", err)
 			continue
 		}
-		go resolveRPC(b, raddr)
+		go nw.resolveRPC(b, raddr)
 	}
 	// unreachable
 }
 
-func (nw *T) send(c *contact.T, msg []byte) (net.UDPConn, error) {
+func (nw *T) send(c *contact.T, msg []byte) (*net.UDPConn, error) {
 	// TODO: contact should probably store the resolved address already
 	// right now it's a string (who wrote this sample code?!)
-	raddr := net.ResolveUDPAddr("udp", c.Address)
+	raddr, err := net.ResolveUDPAddr("udp", c.Address)
+	if err != nil {
+		return nil, err
+		log.Printf("Could not resolve address %v: %v", c.Address, err)
+	}
 	// TODO: would it be safe to use the port we are listening on?
 	// net.Conn docs seem ok with it:
 	// Multiple goroutines may invoke methods on a Conn simultaneously.
-	conn, err = net.DialUDP("udp", nil, raddr)
+	conn, err := net.DialUDP("udp", nil, raddr)
 	if err != nil {
 		log.Printf("Error dialing %v: %v\n", raddr, err)
 		// TODO: do we need to close conn here too? might depend on the error
 		return nil, err
 	}
-	conn.WriteToUDP(msg, raddr)
+	log.Println("DEBUG: write to udp.")
+	_, err = conn.Write(msg)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+	log.Println("DEBUG: write to udp ok.")
 	return conn, nil
 }
 
-func (nw *T) receive(conn net.UDPConn) ([]byte, error) {
+func (nw *T) receive(conn *net.UDPConn) ([]byte, error) {
 	// TODO: make this buffer size configurable somewhere
 	p := make([]byte, 2048)
 	conn.SetReadDeadline(time.Now().Add(nw.timeout))
-	_, err = bufio.NewReader(conn).Read(p)
+	_, err := bufio.NewReader(conn).Read(p)
 	if err != nil {
 		return nil, err
 	}
 	return p, nil
 }
 
-func (nw *T) rpc(c *contact.T, msg *RPC) (map[string]interface{}, error) {
-	b, err := msgpack.Marshal(&msg)
+func (nw *T) rpc(c *contact.T, msg interface{}) (map[string]interface{}, error) {
+	b, err := msgpack.Marshal(msg)
 	if err != nil {
 		log.Printf("Error marshalling FindNode RPC: %v\n", err)
 		return nil, err
 	}
-	conn, err := send(c, b)
+	conn, err := nw.send(c, b)
 	if err != nil {
 		return nil, err
 	}
 	defer conn.Close()
-	rb, err := receive(conn)
+	rb, err := nw.receive(conn)
 	if err != nil {
 		return nil, err
 	}
@@ -124,8 +145,9 @@ func (nw *T) rpc(c *contact.T, msg *RPC) (map[string]interface{}, error) {
 }
 
 func (nw *T) Ping(c *contact.T) error {
-	msg := &RPCPing{rpc_type: PING, sender_id: nw.id}
-	response, err := rpc(c, msg)
+	msg := RPCPing{RPCType: PING, SenderID: *nw.id}
+	log.Println("Sending Ping RPC")
+	_, err := nw.rpc(c, msg)
 	if err != nil {
 		return err
 	}
@@ -134,32 +156,35 @@ func (nw *T) Ping(c *contact.T) error {
 }
 
 func (nw *T) FindNode(c *contact.T, findID *kademliaid.T) ([]contact.T, error) {
-	msg := &RPCFindNode{rpc_type: FIND_NODE, sender_id: nw.id, find_id: *findID}
-	response, err := rpc(c, msg)
+	msg := RPCFindNode{RPCType: FIND_NODE, SenderID: *nw.id, FindID: *findID}
+	response, err := nw.rpc(c, msg)
+	if err != nil {
+		return nil, err
+	}
+	contacts := response["Contacts"].([]contact.T)
+	return contacts, nil
+}
+
+func (nw *T) FindValue(c *contact.T, findID *kademliaid.T) error {
+	msg := RPCFindValue{RPCType: FIND_VALUE, SenderID: *nw.id, FindID: *findID}
+	// response, err := nw.rpc(c, msg)
+	_, err := nw.rpc(c, msg) // just to mute errors until we use response
 	if err != nil {
 		return err
 	}
 	// TODO: do something with response and return
+	return nil
 }
 
-func (nw *T) FindValue(c *contact.T, findID *kademliaid.T) {
-	msg := &RPCFindValue{rpc_type: FIND_VALUE, sender_id: nw.id, find_id: *findID}
-	response, err := rpc(c, msg)
-	if err != nil {
-		return err
-	}
-	// TODO: do something with response and return
-}
-
-func (nw *T) Store(data []byte) error {
-	msg := &RPCStore{rpc_type: STORE, sender_id: nw.id, store_value: data}
+func (nw *T) Store(c *contact.T, data []byte) error {
+	msg := RPCStore{RPCType: STORE, SenderID: *nw.id, StoreValue: data}
 	// not using rpc() since this rpc doesn't need a response
-	b, err := msgpack.Marshal(&msg)
+	b, err := msgpack.Marshal(msg)
 	if err != nil {
 		log.Printf("Error marshalling FindNode RPC: %v\n", err)
 		return err
 	}
-	conn, err := send(c, b)
+	conn, err := nw.send(c, b)
 	if err != nil {
 		return err
 	}
@@ -167,43 +192,54 @@ func (nw *T) Store(data []byte) error {
 	return nil
 }
 
-func resolveRPC(message []byte, raddr *net.UDPAddr) {
-	// TODO: extract the RPC enum from the message
-	args := map[string]interface{}
-	err := msgpack.Unmarshal(b, &args)
-	switch args["rpc_type"] {
+func (nw *T) resolveRPC(message []byte, raddr *net.UDPAddr) {
+	var args map[string]interface{}
+	err := msgpack.Unmarshal(message, &args)
+	if err != nil {
+		log.Printf("Unable to unpack message from %v: %v\n", raddr, err)
+		return
+	}
+	// This depends on what type msgpack decided to encode RPCType with and might break in the future.
+	// TODO: read the RPC type in a safer way
+	rpcType := int(args["RPCType"].(int8))
+	switch rpcType {
 	case PING:
-		pingResponse(raddr)
+		nw.pingResponse(raddr)
 	case FIND_NODE:
-		findNodeResponse(&args, raddr)
+		nw.findNodeResponse(&args, raddr)
 	case FIND_VALUE:
-		findValueResponse(&args, raddr)
+		nw.findValueResponse(&args, raddr)
 	case STORE:
-		storeResponse(&args)
+		nw.storeResponse(&args)
 	default:
-		log.Printf("Unknown RPC: %v\n", args["rpc_type"])
+		log.Printf("Unknown RPC: %v\n", args["RPCType"])
 		// garbage message, don't update routing table
 		return
 	}
 	// TODO: update our routing table
 }
 
-func storeResponse(args *map[string]interface{}) {
+func (nw *T) storeResponse(args *map[string]interface{}) {
 	// no confirmation is sent
 	// TODO: actually store the value from args
 }
 
 func (nw *T) pingResponse(raddr *net.UDPAddr) {
+	log.Println("pingResponse was called")
+	// TODO send a response so the caller doesn't time out
 }
 
-func (nw *T) findValueResponse(raddr *net.UDPAddr)) {
+func (nw *T) findValueResponse(args *map[string]interface{}, raddr *net.UDPAddr) {
 	// TODO: try to find it in the kv store
 	// return
 	// if we can't find it, just treat it as a FindNode
-	nw.findNodeResponse(c, findID)
+	nw.findNodeResponse(args, raddr)
 }
 
-func (nw *T) findNodeResponse(raddr *net.UDPAddr) {
-	target := (*args)["find_id"].(kademliaid.T)
-	nw.routingtable.FindClosestContacts(&target)
+func (nw *T) findNodeResponse(args *map[string]interface{}, raddr *net.UDPAddr) {
+	target := (*args)["FindID"].(kademliaid.T)
+	// TODO: make it the responsibility of routingtable to decide how many
+	// contacts we get back
+	log.Println("WARN: number of contacts to return should not be my problem")
+	nw.routingtable.FindClosestContacts(&target, 20)
 }
--- network/network_test.go
+++ network/network_test.go
@@ -2,25 +2,39 @@ package network
 
 import (
 	"testing"
+	"time"
+	"log"
 	"github.com/mjolnir92/kdfs/kademliaid"
 	"github.com/mjolnir92/kdfs/contact"
 )
 
-func TestHello(t *testing.T) {
-	t.Error("hello test")
+func TestPingExternalServer(t *testing.T) {
+	// create a contact for the external server
+	id_server := kademliaid.New("0000000000000000000000000000000000000000")
+	ct_server := contact.New(id_server, "localhost:10001")
+	// client
+	id_client := kademliaid.New("1000000000000000000000000000000000000000")
+	nw_client := New(5000, id_client)
+	// Send the ping and wait for a response
+	log.Println("Sending ping to external server")
+	nw_client.Ping(&ct_server)
+	log.Println("it is done.")
 }
 
 func TestPing(t *testing.T) {
-	id_server := &kademliaid.New("0000000000000000000000000000000000000000")
-	nw_server := New(150, id_server)
-	ct_server := &New(id_server, "localhost:12300")
-	go nw_server.Listen("localhost", "12300")
+	id_server := kademliaid.New("0000000000000000000000000000000000000000")
+	nw_server := New(5000, id_server)
+	ct_server := contact.New(id_server, "localhost:12300")
+	go nw_server.Listen("localhost", 12300)
 	// TODO: clean up the Listen goroutine
-	id_client := &kademliaid.New("1000000000000000000000000000000000000000")
-	nw_client := New(150, id_client)
-	err := nw_client.Ping(ct_server)
+	id_client := kademliaid.New("1000000000000000000000000000000000000000")
+	nw_client := New(5000, id_client)
+	// Wait a bit so the server is ready
+	time.Sleep(50 * time.Millisecond)
+	// Send the ping and wait for a response
+	err := nw_client.Ping(&ct_server)
 	if err != nil {
-		t.Error("Ping returned an error:", v)
+		t.Error("Ping returned an error:", err)
 	}
 	// TODO: was the routing table updated?
 }
