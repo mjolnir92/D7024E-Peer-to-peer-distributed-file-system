e4668c08e41c358d9808d001f25cd40fd5ed32e9
norric <Riwzi@users.noreply.github.com>
2017-09-24 13:41:34 +0200

    make value fields public, NewValue returns Value instead of *Value

--- kvstore/value.go
+++ kvstore/value.go
@@ -7,29 +7,29 @@ import (
 //timestamp indicates when the key-value pair was last stored/updated?
 //pin indicates whether the stored file is pinned
 type Value struct {
-	timestamp time.Time
-	pin bool
-	data []byte
+	Timestamp time.Time
+	Pin bool
+	Data []byte
 }
 
 
-func NewValue(pin bool, data []byte) *Value {
-	v := &Value{}
-	v.timestamp = time.Now()
-	v.pin = pin
-	v.data = data
+func NewValue(pin bool, data []byte) Value {
+	v := Value{}
+	v.Timestamp = time.Now()
+	v.Pin = pin
+	v.Data = data
 	return v
 }
 
 func (v *Value) GetData() []byte {
-	return v.data
+	return v.Data
 }
 
 func (v *Value) GetPin() bool {
-	return v.pin
+	return v.Pin
 }
 
 //Returns true if v's timestamp is earlier than u's timestamp
 func (v *Value) Before(u Value) bool {
-	return v.timestamp.Before(u.timestamp)
+	return v.Timestamp.Before(u.Timestamp)
 }
