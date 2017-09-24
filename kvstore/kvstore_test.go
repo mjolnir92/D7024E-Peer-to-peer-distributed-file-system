7710e548e1ec870d9b4760fea75fbeedc23e1e2c
norric <Riwzi@users.noreply.github.com>
2017-09-24 13:43:43 +0200

    rewrite kvstore tests

--- kvstore/kvstore_test.go
+++ kvstore/kvstore_test.go
@@ -2,7 +2,6 @@ package kvstore
 
 import (
 	"testing"
-	"time"
 	"github.com/mjolnir92/kdfs/kademliaid"
 )
 
@@ -10,39 +9,17 @@ func TestKVStore(t *testing.T) {
 	kv := New()
 	data := []byte("data")
 	id := kademliaid.NewHash(data)
-	v := Value{timestamp: time.Now(), pin: true, data: data}
+	v := NewValue(true, data)
 
 	kv.Store(v)
 	//Check if the key-value pair exists in the store (we can retrieve it)
 	if _, ok := kv.Get(*id); !ok {
 		t.Error("TestKVStore failed, key-value pair did not get stored")
 	}
-}
 
-func TestKVRemove(t *testing.T) {
-	kv := New()
-	data := []byte("data")
-	id := kademliaid.NewHash(data)
-	v := Value{timestamp: time.Now(), pin: true, data: data}
-
-	kv.Store(v)
 	kv.Remove(v)
 	//Check if the key-value pair exists after removing it
 	if _, ok := kv.Get(*id); ok {
-		t.Error("TestKVRemove failed, key-value pair still exists")
-	}
-}
-
-func TestKVGet(t *testing.T) {
-	kv := New()
-	data := []byte("data")
-	id := kademliaid.NewHash(data)
-	v := Value{timestamp: time.Now(), pin: true, data: data}
-
-	kv.Store(v)
-	//Check if the key-value pair exists in the store (we can retrieve it)
-	//Can't directly compare two structs containing a []byte
-	if _, ok := kv.Get(*id); !ok {
-		t.Error("TestKVGet failed, couldnn't retrieve the value")
+		t.Error("TestKVStore failed, key-value pair did not get removed")
 	}
 }
